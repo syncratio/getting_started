cab1b16c51f6b0057463ff64f64b4e772b6b0939
syncratio <syncratio@gmail.com>
2022-11-14 22:30:12 +0200

    fooling around + basic structure

---
+++ main.js
@@ -0,0 +1,23 @@
+function main() {
+    let username = promptForUsername();
+    let message = getUserSpecificMessage(username);
+    const messageElement = document.getElementById("message");
+    messageElement.innerHTML = message;
+    console.log(`You are finally awake, ${username}`);
+}
+
+function getUserSpecificMessage(username) {
+    if(username === "SidFj") {
+        return "You just lost $500!";
+    }
+    let message = "Hello " + username;
+    return message;
+}
+
+function promptForUsername() {
+    let username = prompt("Please enter your username");
+    return username;
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+    main();
+});
