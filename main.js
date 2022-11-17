c670aae95452c1fdc7645f5243fc54fda4f08be3
Sargei Naumov <naumov.200001@gmail.com>
2022-11-17 20:26:28 +0300

    added: button, images

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ function getUserSpecificMessage(username) {
     if(username === "SidFj") {
         return "You just lost $500!";
     }
+    if(username === "syncratio") {
+        return "You just lost your time!";
+    }
     let message = "Hello, " + username;
     return message;
 }
@@ -18,6 +21,11 @@ function promptForUsername() {
     let username = prompt("Please enter your username") || 'Guest';
     return username;
 }
+
+function todayWeCook() {
+    document.getElementById("saul").src = "https://media.tenor.com/KEuepKkr80UAAAAd/breaking-bad-better-call-saul.gif";
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     main();
 });
