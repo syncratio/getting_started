8852d1fb64a066cd1ea9b089f292d6d2189f396c
Volodymyr Hanzenko <volodymyr.hanzenko@onereach.com>
2022-12-10 22:32:11 +0200

    feature: added server-like behavior

---
+++ server-mock.js
@@ -0,0 +1,70 @@
+function initStorage() {
+    let users = [];
+    let json = JSON.stringify(users);
+    localStorage.setItem("users", json);
+}
+
+function getUsers(callback) {
+    let json = localStorage.getItem("users");
+    let users = [];
+    try {
+        users = JSON.parse(json);
+        users = users.map(({ id, email }) => ({ id, email }));
+    } catch (e) {
+        console.log("Error parsing JSON");
+    }
+    setTimeout(() => {
+        callback(users);
+    }, 500);
+}
+
+function getUserInfo(id, callback) {
+    let json = localStorage.getItem("users");
+    let users = [];
+    try {
+        users = JSON.parse(json);
+    } catch (e) {
+        console.log("Error parsing JSON");
+    }
+    let user = users.find(user => user.id === id);
+    setTimeout(() => {
+        callback(user);
+    }, 500);
+}
+
+function validateUser(data) {
+    return data.firstName && data.lastName && data.email && data.password;
+}
+
+function createUser(data, callback) {
+    let json = localStorage.getItem("users");
+
+    let users = [];
+    try {
+        users = JSON.parse(json);
+    } catch (e) {
+        console.log("Error parsing JSON");
+    }
+
+    if (!validateUser(data)) {
+        setTimeout(() => {
+            callback(false);
+        }, 500);
+        return;
+    }
+
+    data.id = users.length + 1;
+    data.created = new Date();
+    users.push(data);
+    localStorage.setItem("users", JSON.stringify(users));
+
+    setTimeout(() => {
+        callback(true);
+    }, 500);
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    initStorage();
+});
+
+
