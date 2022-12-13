6a3ed85f59932126a0a00172ebc79d1c2b6ce3c5
SidFj <naumov.200001@gmail.com>
2022-12-13 19:37:22 +0300

    feature: form submit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-getUsers(console.log);
+const signUp = document.querySelector(".sign-up-button");
+const userForm = document.querySelector(".user-form");
+
+userForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    createUser(formData, function(result) {
+        console.log(result);
+    });
+})
+
+signUp.addEventListener("click", function(event) { 
+    console.log("clicked");
+    console.log(event);
+})
+
+
