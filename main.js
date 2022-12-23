04ac43b0fa3e0dd9b4556714815b8548c03d50f2
Sargei Naumov <naumov.200001@gmail.com>
2022-12-23 03:01:36 +0300

    feature: sign up message

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const signUp = document.querySelector(".sign-up-button");
 const userForm = document.querySelector(".user-form");
+const msg = document.querySelector(".msg");
 
 userForm.addEventListener("submit", function(event) {
     event.preventDefault();
+    signUp.setAttribute("disabled", true);
+    signUp.innerHTML = "Loading...";
+    msg.classList.add("hidden");
     const formData = new FormData(event.target);
-    createUser(formData, function(result) {
-        console.log(result);
+    createUser(formData, (result) => {
+        signUp.removeAttribute("disabled");
+        signUp.innerHTML = "Sign Up";
+        if (!result) {
+            msg.classList.replace("success-msg", "error-msg");
+            msg.classList.remove("hidden");
+            msg.innerHTML = "User creation failed. Check if the form is filled correctly.";
+        } else {
+            msg.classList.add("error-msg", "success-msg");
+            msg.classList.remove("hidden");
+            msg.innerHTML = "User creation succeeded.";
+        }
     });
 })
-
-signUp.addEventListener("click", function(event) { 
-    console.log("clicked");
-    console.log(event);
-})
-
-
